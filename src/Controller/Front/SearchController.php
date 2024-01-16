23c8415e507ccbc989a1ce47a9ec1f307a62d9c5
Rauum <vache.romain30@gmail.com>
2024-01-16 20:11:20 +0100

    Début système recherche

--- src/Controller/Front/SearchController.php
+++ src/Controller/Front/SearchController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller\Front;
 
+use App\Form\SearchFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-    #[Route('/', name: 'app_search')]
-    public function index(): Response
+    #[Route('/search', name: 'app_search')]
+    public function search(Request $request): Response
     {
-        return $this->render('front/search/index.html.twig', [
+        $form = $this->createForm(SearchFormType::class);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupérer les données du formulaire
+            $data = $form->getData();
+            var_dump($data);
+        }
+
+        return $this->render('front/search/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
+
+
 }
