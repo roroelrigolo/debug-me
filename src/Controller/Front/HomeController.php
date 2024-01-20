47f181e7b30f1913f9c5ae1423f0bce055e40b00
Adem <ademduran6888@gmail.com>
2024-01-20 17:31:53 +0100

    feat(controllers): getting users and tickets for the home page

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Front;
 
 use App\Repository\TagRepository;
+use App\Repository\UserRepository;
+use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(TagRepository $tagRepository): Response
+    public function index(TagRepository $tagRepository, UserRepository $userRepository, TicketRepository $ticketRepository): Response
     {
         $tags = $tagRepository->findAll();
+        $users = $userRepository->findBy([],['points'=>'DESC'], 3);
+        $tickets = $ticketRepository->findBy([], [], 4);
 
         return $this->render('front/home/index.html.twig', [
             'tags' => $tags,
+            'users' => $users,
+            'tickets' => $tickets,
         ]);
     }
 }
