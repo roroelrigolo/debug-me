bd76cfd7914022d92efe4ad1bfaa3e651e17f539
Adem <ademduran6888@gmail.com>
2024-01-24 00:32:51 +0100

    fix(controller): providing users and tickets list

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller\Front;
 
-use App\Repository\TagRepository;
 use App\Repository\UserRepository;
 use App\Repository\TicketRepository;
 use App\Form\SearchFormType;
@@ -14,8 +13,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserRepository $userRepository, TicketRepository $ticketRepository): Response
     {
+
+        $users = $userRepository->findBy([],['points'=>'DESC'], 3);
+        $tickets = $ticketRepository->findBy([], [], 4);
 
         $form = $this->createForm(SearchFormType::class);
         $form->handleRequest($request);
@@ -33,6 +35,8 @@ class HomeController extends AbstractController
         }
 
         return $this->render('front/home/index.html.twig', [
+            'users' => $users,
+            'tickets' => $tickets,
             'form' => $form->createView(),
         ]);
     }
