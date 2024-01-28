97dfb09ff9a4e075b35d40836dc449c9f99df9c1
Adem <ademduran6888@gmail.com>
2024-01-28 19:30:53 +0100

    fix(tables): adding hover on table rows

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,7 @@ module.exports = {
       boxShadow: {
         'ticket': '0px 1px 0px 0px rgba(0, 0, 0, 0.50)',
         'ticket-hover': '0px 4px 0px 0px rgba(0, 0, 0, 0.50)',
+        'table': 'inset 2px 0 0 black, inset -2px 0 0 black'
       }
     },
   },
